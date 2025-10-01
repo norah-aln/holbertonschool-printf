93380f8a988bf8acd3738c3c3c51d56760518fde
norah-aln <zylayo@gmail.com>
2025-10-01 20:27:45 +0000

    Fix Betty style - split function into smaller functions

--- _printf.c
+++ _printf.c
@@ -1,6 +1,66 @@
 #include "main.h"
 
 /**
+ * print_char - prints a character
+ * @args: arguments list
+ *
+ * Return: 1
+ */
+int print_char(va_list args)
+{
+	_putchar(va_arg(args, int));
+	return (1);
+}
+
+/**
+ * print_string - prints a string
+ * @args: arguments list
+ *
+ * Return: number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+	while (*str)
+	{
+		_putchar(*str);
+		str++;
+		count++;
+	}
+	return (count);
+}
+
+/**
+ * handle_specifier - handles format specifiers
+ * @format: format character
+ * @args: arguments list
+ *
+ * Return: number of characters printed
+ */
+int handle_specifier(char format, va_list args)
+{
+	if (format == 'c')
+		return (print_char(args));
+	else if (format == 's')
+		return (print_string(args));
+	else if (format == '%')
+	{
+		_putchar('%');
+		return (1);
+	}
+	else
+	{
+		_putchar('%');
+		_putchar(format);
+		return (2);
+	}
+}
+
+/**
  * _printf - produces output according to a format
  * @format: format string
  *
@@ -10,8 +70,6 @@ int _printf(const char *format, ...)
 {
 	int count = 0, i = 0;
 	va_list args;
-	char *str;
-	char c;
 
 	if (format == NULL)
 		return (-1);
@@ -23,35 +81,7 @@ int _printf(const char *format, ...)
 			if (format[i + 1] == '\0')
 				return (-1);
 			i++;
-			if (format[i] == 'c')
-			{
-				c = va_arg(args, int);
-				_putchar(c);
-				count++;
-			}
-			else if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(null)";
-				while (*str)
-				{
-					_putchar(*str);
-					str++;
-					count++;
-				}
-			}
-			else if (format[i] == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-			else
-			{
-				_putchar('%');
-				_putchar(format[i]);
-				count += 2;
-			}
+			count += handle_specifier(format[i], args);
 		}
 		else
 		{
