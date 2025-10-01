c27e0337f8d58e223bb814a4dc91469caeaa2761
norah-aln <zylayo@gmail.com>
2025-10-01 15:29:43 +0000

    Fix: remove test mains, add prototypes to main.h, remove executables

--- main.h
+++ main.h
@@ -4,7 +4,14 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+/* main functions */
 int _printf(const char *format, ...);
 int _putchar(char c);
 
-#endif
+/* helper functions used in _printf.c (prototypes required) */
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int handle_format(char format, va_list args);
+
+#endif /* MAIN_H */
