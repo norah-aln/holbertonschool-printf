7a4af655d84fc74456b40851a351e2b3460d2d18
norah-aln <zylayo@gmail.com>
2025-10-01 14:58:34 +0000

    Fix _printf implementation - handle all test cases

---
+++ README.md
@@ -0,0 +1,15 @@
+# _printf Project
+
+Custom implementation of printf function in C.
+
+## Description
+This project implements a simplified version of the printf function that handles basic format specifiers.
+
+## Supported Format Specifiers
+- %c: character
+- %s: string
+- %%: percent sign
+
+## Authors
+- Norah Mohammed Alnujidi
+- Layan Saleem Aldosari
--- _printf.c
+++ _printf.c
@@ -1,8 +1,85 @@
 #include "main.h"
+/**
+ * print_char - prints a character
+ * @args: va_list containing the character
+ *
+ * Return: number of characters printed (1)
+ */
+int print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	_putchar(c);
+	return(1);
+}
+/**
+ * print_string - prints a string
+ * @args: va_list containing the string
+ *
+ * Return: number of characters printed
+ */
+int print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	if (str == NULL)
+		str = "(null)";
+
+	while (str[count] != '\0')
+	{
+		_putchar(str[count] != '\0');
+                count++;
+	}
+
+	return (count);
+}
 
 /**
+ * print_percent - prints a percent sign
+ * @args: va_list (unused)
+ *
+ * Return: number of characters printed (1)
+ */
+int print_percent(va_list args)
+{
+	(void)args;
+	_putchar('%');
+	return (1);
+}
+
+/**
+ * handle_format - handles format specifiers
+ * @format: format character
+ * @args: va_list of arguments
+ *
+ * Return: number of characters printed, or 0 if unknown
+ */
+int handle_format(char format, va_list args)
+{
+	int i;
+	struct format_spec {
+		char specifier;
+		int (*func)(va_list);
+	} specs[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'\0', NULL}
+	};
+	for (i = 0; specs[i].specifier != '\0'; i++)
+	{
+		if (format == specs[i].specifier)
+			return (specs[i].func(args));
+	}
+
+	_putchar('%');
+	_putchar(format);
+	return (2);
+}
+/**
  * _printf - produces output according to a format
- * @format: format string containing directives
+ * @format: format string
  *
  * Return: number of characters printed
  */
@@ -21,36 +98,13 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
+			if (format[i + 1] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
 			i++;
-			if (format[i] == '\0')
-				break;
-
-			if (format[i] == '%')
-			{
-				_putchar('%');
-				count++;
-			}
-			else if (format[i] == 'c')
-			{
-				char c = va_arg(args, int);
-				_putchar(c);
-				count++;
-			}
-			else if (format[i] == 's')
-			{
-				char *str = va_arg(args, char *);
-				int j = 0;
-
-				if (str == NULL)
-					str = "(null)";
-
-				while (str[j] != '\0')
-				{
-					_putchar(str[j]);
-					count++;
-					j++;
-				}
-			}
+			count += handle_format(format[i], args);
 		}
 		else
 		{
